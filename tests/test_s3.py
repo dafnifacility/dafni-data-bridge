d9520be1ee92e2eb73fb1d5c1ce8fe116f108da5
Muhammad <muhammad.mohsin@stfc.ac.uk>
2026-02-23 11:05:17 +0000

    Core 5181 create unit tests

---
+++ tests/test_s3.py
@@ -0,0 +1,66 @@
+import os
+import sys
+from unittest.mock import patch
+
+import boto3
+from main import main
+from moto import mock_aws
+
+
+@mock_aws
+def test_s3_upload_http(mock_file):
+    with patch.dict(os.environ, {"MOTO_S3_CUSTOM_ENDPOINTS": "https://s3.test.com"}):
+        s3 = boto3.resource("s3", region_name="us-east-1")
+        bucket = "testbucket"
+        s3.create_bucket(Bucket=bucket)
+
+        test_args = [
+            "main.py",
+            "--token",
+            "mock-token-abc",
+            "--url",
+            "https://test.url.ac.uk/dir/file.nc",
+            "--dest",
+            f"https://{bucket}.s3.test.com/files",
+            "--checksum",
+        ]
+
+        with patch.object(sys, "argv", test_args):
+            main()
+
+        obj = s3.Object(bucket, "files/file.nc")
+        uploaded_content = obj.get()["Body"].read()
+
+    assert uploaded_content == b"fake netcdf data"
+
+
+@mock_aws()
+def test_s3_upload_ftp(mock_ftp_server, mock_file):
+    host, port = mock_ftp_server
+    url = f"ftp://{host}:{port}/file.nc"
+
+    with patch.dict(os.environ, {"MOTO_S3_CUSTOM_ENDPOINTS": "https://s3.test.com"}):
+        s3 = boto3.resource("s3", region_name="us-east-1")
+        bucket = "testbucket"
+        s3.create_bucket(Bucket=bucket)
+
+        test_args = [
+            "main.py",
+            "--username",
+            "anonymous",
+            "--password",
+            "user@email.com",
+            "--url",
+            url,
+            "--dest",
+            f"https://{bucket}.s3.test.com/files",
+            "--checksum",
+        ]
+
+        with patch.object(sys, "argv", test_args):
+            main()
+
+        obj = s3.Object(bucket, "files/file.nc")
+        uploaded_content = obj.get()["Body"].read()
+
+    assert uploaded_content == b"mocked ftp file.nc"
