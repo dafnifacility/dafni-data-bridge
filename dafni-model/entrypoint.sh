e8070ddf35a134a40e4e5f884be8142ad7901837
Muhammad Mohsin <muhammad.mohsin@stfc.ac.uk>
2026-03-18 12:33:57 +0000

    push testig changes

--- dafni-model/entrypoint.sh
+++ dafni-model/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+if [ -f /data/inputs/args.env ]; then
+  export $(grep -v '^#' /data/inputs/args.env | xargs)
+fi
+
 ceda-download-tool \
-    --username "$USERNAME" 
-    --p\
+    --username "$USERNAME" \
+    --password "$PASSWORD" \
     --url "$URL" \
     --dest "/data/outputs/" \
-    --checksum --debug
+    --checksum 
