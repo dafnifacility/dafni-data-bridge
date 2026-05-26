bc52d9dbfe27c97935a0c4e255b44a4c48756a26
Muhammad Mohsin <muhammad.mohsin@stfc.ac.uk>
2026-05-26 13:16:25 +0100

    move remote_upload dir for clarity

---
+++ dataset_download_tool/remote_upload/__init__.py
@@ -0,0 +1,6 @@
+# from dataset_download_tool.remote_upload.s3_upload import S3Client
+
+# def get_uploader(endpoint_url: str):
+
+#     if endpoint_url.__contains__("s3"):
+#         return S3Client(endpoint_url)
